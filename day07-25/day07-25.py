940e892ab7c42dde0eee5da01e21ad8067f7c6fc
Arno Kamphuis <ik@arnokamphuis.nl>
2025-03-23 11:40:31 +0100

    Finished day 7 of 2025

--- day07-25/day07-25.py
+++ day07-25/day07-25.py
@@ -8,18 +8,39 @@ if len(sys.argv) > 3:
 else:
     runs = 1
 
-text_file = open(f"dayxx-{runtype}.txt", "r")
+text_file = open(f"day07-{runtype}.txt", "r")
 
 lines = [line.split() for line in text_file.readlines()]
+idx = 0
+while lines[idx] != []:
+    idx += 1
+
+freqs = [int(x[0]) for x in lines[:idx]]
+swaps = [list(map(int,x[0].split('-'))) for x in lines[idx+1:-2]]
+test  = int(lines[-1][0])
 
 def part1():
-    return 0
+    new_freqs = freqs.copy()
+    for swap in swaps:
+        new_freqs[swap[0]-1], new_freqs[swap[1]-1] = new_freqs[swap[1]-1], new_freqs[swap[0]-1]
+    return new_freqs[test-1]
 
 def part2():
-    return 0
+    new_freqs = freqs.copy()
+    combs = zip(swaps, [*swaps[1:], swaps[0]])
+    for swap1, swap2 in combs:
+        new_freqs[swap1[1]-1], new_freqs[swap2[0]-1], new_freqs[swap1[0]-1] = new_freqs[swap1[0]-1], new_freqs[swap1[1]-1], new_freqs[swap2[0]-1]
+    return new_freqs[test-1]
 
 def part3():
-    return 0
+    sorted_swaps = [sorted(s) for s in swaps]
+    new_freqs = freqs.copy()
+    n = len(freqs)
+    for swap in sorted_swaps:
+        swap_size = min(n - swap[1] + 1, swap[1] - swap[0])
+        for idx in range(swap_size):
+            new_freqs[swap[0]+idx-1], new_freqs[swap[1]+idx-1] = new_freqs[swap[1]+idx-1], new_freqs[swap[0]+idx-1]
+    return new_freqs[test-1]
 
 if runpart == 1 or runpart == 0:
     for run in range(runs):
